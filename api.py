2cad16591ea41ac79242833bd1d6cbc427bd2291
rafinhafs07-ux <rafinhafs07@gmail.com>
2026-03-24 17:04:43 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
-API_KEY = "SUA_API_KEY"
+API_KEY = os.getenv("API_KEY")
 
 def get_players_data():
-    url = "https://api-nba-v1.p.rapidapi.com/players/statistics"
+    url = "https://api-nba-v1.p.rapidapi.com/players/statistics?season=2023"
 
     headers = {
         "X-RapidAPI-Key": API_KEY,
@@ -11,18 +12,28 @@ def get_players_data():
     }
 
     response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("Erro na API:", response.status_code)
+        return []
+
     data = response.json()
 
     jogadores = []
 
-    try:
-        for item in data["response"]:
-            jogadores.append({
+    for item in data.get("response", []):
+        try:
+            stats = item["statistics"][0]
+
+            jogador = {
                 "name": item["player"]["firstname"] + " " + item["player"]["lastname"],
-                "threePointAttempts": item["statistics"][0]["fg3a"],
-                "threePointPct": item["statistics"][0]["fg3p"] / 100
-            })
-    except:
-        return []
+                "threePointAttempts": stats.get("fg3a", 0),
+                "threePointPct": stats.get("fg3p", 0) / 100
+            }
+
+            jogadores.append(jogador)
+
+        except Exception as e:
+            continue
 
     return jogadores
