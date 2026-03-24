4116caf51fa8e80a83881ea04b3a84f076dc4b55
rafinhafs07-ux <rafinhafs07@gmail.com>
2026-03-24 17:17:17 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,7 +2,11 @@ def analisar_jogadores(jogadores):
     under_players = []
 
     for j in jogadores:
-        if j["threePointAttempts"] < 4 and j["threePointPct"] < 0.32:
+        if (
+            j["threePointAttempts"] < 4 and
+            j["threePointPct"] < 0.32 and
+            j["minutes"] > 10  # evita jogadores irrelevantes
+        ):
             under_players.append(j)
 
     return under_players
