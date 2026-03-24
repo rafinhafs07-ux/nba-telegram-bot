ab35d1863125bf9402f03e9f694d8c329109ca7d
rafinhafs07-ux <rafinhafs07@gmail.com>
2026-03-24 16:01:05 -0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = os.getenv("8612950541:AAGZ7hWSZu1mS17lv7ubQVwzhPIvNTgT_0c")
+
+def start(update, context):
+    update.message.reply_text("🤖 Bot da NBA está online!")
+
+def analise(update, context):
+    update.message.reply_text("📊 Análise em breve...")
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("analise", analise))
+
+updater.start_polling()
+updater.idle()
